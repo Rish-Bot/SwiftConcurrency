57c13148380398cb9f1ab8f7bcba64b21f764684
Harish <115090388+Rish-Bot@users.noreply.github.com>
2024-12-02 13:38:06 +0530

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Hari on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
