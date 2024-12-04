ee82ae9b15b5110805a7cbd406781eb4000cf560
Harish <harishanbazhagan.s@gmail.com>
2024-12-04 16:15:06 +0530

    Add DownloadImgAsyncDatamanager Add ViewModel Add downlaodImageWithCompletionhandler Add updates in UI for Button to fetch the data

--- SwiftConcurrency/DownloadImgAsync.swift
+++ SwiftConcurrency/DownloadImgAsync.swift
@@ -7,11 +7,82 @@
 
 import SwiftUI
 
-struct DownloadImgAsync: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//DataManager / Imageloader
+class DownloadImgAsyncDataManager {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func downlaodImageWithCompletionHandler(completionHandler:@escaping (UIImage?, Error?) -> ()){
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                let image = UIImage(data: data),
+                let resonse = response as? HTTPURLResponse,
+                resonse.statusCode >= 200 && resonse.statusCode < 300,
+                error == nil
+            else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+    
+}
+
+//viewModel
+class DownloadImgAsyncVM: ObservableObject {
+    @Published var image: UIImage?
+    let dataManager: DownloadImgAsyncDataManager = DownloadImgAsyncDataManager()
+    
+    func fetchImage() {
+        self.image = UIImage(systemName: "heart.fill")
+    }
+    
+    func fetchImageWithCompletionHandler() {
+        self.dataManager.downlaodImageWithCompletionHandler {[weak self] image, error in
+            
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+            
+        }
     }
 }
+
+struct DownloadImgAsync: View {
+    @StateObject private var viewModel = DownloadImgAsyncVM()
+    var body: some View {
+        VStack{
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.green,.blue,.red)
+                    .frame(width: 300, height: 400)
+                    .padding()
+            }
+            
+            Button("New Image") {
+                viewModel.fetchImageWithCompletionHandler()
+            }.padding()
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .onAppear {
+            viewModel.fetchImageWithCompletionHandler()
+        }
+        
+        
+    }
+}
+
 
 #Preview {
     DownloadImgAsync()
