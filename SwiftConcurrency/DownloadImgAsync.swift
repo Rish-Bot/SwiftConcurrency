9de5e7f2f5f94d00563a31de8636d42a804d7f79
Harish <harishanbazhagan.s@gmail.com>
2024-12-04 15:48:16 +0530

    Initialize DownlaodImgAsync View

---
+++ SwiftConcurrency/DownloadImgAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadImgAsync.swift
+//  SwiftConcurrency
+//
+//  Created by Hari on 04/12/24.
+//
+
+import SwiftUI
+
+struct DownloadImgAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadImgAsync()
+}
