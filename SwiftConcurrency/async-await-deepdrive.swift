420a435cbae7837b73442abe0124ea976a8d81cb
Harish <harishanbazhagan.s@gmail.com>
2024-12-05 23:34:10 +0530

    add viewmodel and update in async-await-deepdive

--- SwiftConcurrency/async-await-deepdrive.swift
+++ SwiftConcurrency/async-await-deepdrive.swift
@@ -6,21 +6,47 @@
 //
 
 import SwiftUI
-class asyncAwaitDeepdriveVM : ObservableObject {
+class asyncAwaitDeepdiveVM : ObservableObject {
     @Published var dataArray : [String] = []
+    
+    func addData(){
+        dataArray.append("Welcome to deepDive into Async-Await")
+    }
+    
+    func addAuthor1() async throws {
+        dataArray.append("Author1")
+        
+    }
 }
-struct async_await_deepdrive: View {
-    @StateObject private var viewModel =  asyncAwaitDeepdriveVM()
+
+struct async_await_deepdive: View {
+    @StateObject private var viewModel =  asyncAwaitDeepdiveVM()
     var body: some View {
         List{
             ForEach(viewModel.dataArray, id: \.self) { data in
                 Text(data)
             }
         }
+        .onAppear{
+            viewModel.addData()
+            Task{
+                try? await viewModel.addAuthor1()
+            }
+        }
+        
         
     }
 }
 
 #Preview {
-    async_await_deepdrive()
+    async_await_deepdive()
 }
+
+/*
+ Notes: Async-Await
+ It's more modern in ios concurrency you often see these terms additionally you could see the Task & error handling part inside it.
+ 
+ Since both the Async-Await is there it's no necessary it always run in the main thread some action may run in background thread also.
+ 
+ 
+ */
