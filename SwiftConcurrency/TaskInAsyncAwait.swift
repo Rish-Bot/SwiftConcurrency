78238a8d3dfe6aa804c88c04ab3f8df5e0ac1cbc
Harish <harishanbazhagan.s@gmail.com>
2024-12-07 23:04:58 +0530

    Initilalize task for learning

---
+++ SwiftConcurrency/TaskInAsyncAwait.swift
@@ -0,0 +1,28 @@
+//
+//  TaskInAsyncAwait.swift
+//  SwiftConcurrency
+//
+//  Created by Hari on 07/12/24.
+//
+
+import SwiftUI
+
+struct TaskInAsyncAwait: View {
+    var body: some View {
+        
+        VStack{
+            
+        }.onAppear{
+            Task{
+                
+            }
+        }
+        
+        
+    }
+        
+}
+
+#Preview {
+    TaskInAsyncAwait()
+}
