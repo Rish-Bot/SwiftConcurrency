e1348f16c04932b5926ca76473ed5d78e8301db9
Harish <harishanbazhagan.s@gmail.com>
2024-12-09 22:40:58 +0530

    update the task notes : computed property async, useful task api's :[I worked on the office project today]

--- SwiftConcurrency/TaskInAsyncAwait.swift
+++ SwiftConcurrency/TaskInAsyncAwait.swift
@@ -29,10 +29,29 @@ struct TaskInAsyncAwait: View {
 
 
 /*
- Notes:)
+ Notes :)
  >>  Task yield : it's a wait yield and execute next all the things
  >>  You can add the detach task to add the detachment to the praent task
  >>  Don't use the detach task if it's possible.
  >>  Task cancel also a great way to cancel suppose if the call is working when it's not necessary at all -
- >> .task{ } in SwiftUI automatically cancel task if the view disappears automatically. 
+ >> .task{ } in SwiftUI automatically cancel task if the view disappears automatically.
+ >> check for cancellation for Cancellation() to externally somethings goes wrong on the app at any point of time.
+ 
+ >> task comes with bunch of API's most useful
+    1. Task.sleep
+    2. Task.yield
+    3. Task.cancel
+    4. Task.iscancelled
+    5. Task.value
+    6. Task.detached
+ 
+ >> Async let creates a promises and wait till all the task get complete and give you the value if you want multiple task result at the time you can go with it
+ 
+ >> You can add the async await to a computed property by Keeping async in the get property of computed one
+    Example:
+        var computedProperty : Organisation {
+            get async {
+                await networkcall
+            }
+        }
  */
